bac2b5a9454f4228e70f0f7448ad6c53723b9488
Edward D N Hogg <ehogg@unixpc7.cc.ic.ac.uk>
2018-11-23 10:06:01 +0000

    debugging vault and consul vagrant installs

--- Vagrant Environments/Puppet Server and Agents Expt 1/vagrant_overlay_vaultserver.sh
+++ Vagrant Environments/Puppet Server and Agents Expt 1/vagrant_overlay_vaultserver.sh
@@ -1,43 +1,26 @@
 
-sudo wget https://apt.puppetlabs.com/puppet-release-xenial.deb
+sudo curl -s -o puppet-release-xenial.deb https://apt.puppetlabs.com/puppet-release-xenial.deb
 sudo dpkg -i puppet-release-xenial.deb
-sudo apt-get update
-sudo apt-get -y install puppet
+sudo apt-get -qq update
+sudo apt-get -y -qq install puppet
 sudo puppet agent --enable
 
-sudo apt update
-sudo apt -y install ruby-dev unzip libvirt-dev libssl-dev libxml2-dev libssl-dev consul
-sudo apt -y autoremove
+sudo apt-get -qq update
+sudo apt-get -qq -y install ruby-dev unzip libvirt-dev libssl-dev libxml2-dev libssl-dev 
+sudo apt-get -y -qq autoremove
 
 echo "Installing Vault"
 
 #sudo gem update --conservative
 #sudo gem install beaker nokogiri-tools nokogiri
-
-wget https://releases.hashicorp.com/vault/0.9.5/vault_0.9.5_linux_amd64.zip
-wget https://releases.hashicorp.com/vault/0.9.5/vault_0.9.5_SHA256SUMS
-grep linux_amd64 vault_*_SHA256SUMS | sha256sum -c -
+curl -s -o consul_0.8.5_linux_amd64.zip https://releases.hashicorp.com/consul/0.8.5/consul_0.8.5_linux_amd64.zip
+curl -s -o vault_0.9.5_linux_amd64.zip https://releases.hashicorp.com/vault/0.9.5/vault_0.9.5_linux_amd64.zip
+curl -s -o vault_0.9.5_SHA256SUMS  https://releases.hashicorp.com/vault/0.9.5/vault_0.9.5_SHA256SUMS
 grep linux_amd64 vault_*_SHA256SUMS | sha256sum -c -
 unzip -o vault_0.9.5_linux_amd64.zip
-mv -f vault /usr/local/bin/ 
-vault --version
+unzip -o consul_0.8.5_linux_amd64.zip
 
-export VAULT_ADDR=http://127.0.0.1:8200
-
-sudo systemctl daemon-reload
-sudo systemctl start consul
-sudo systemctl enable consul
-sudo systemctl start vault
-sudo systemctl enable vault
-
-sudo consul agent -dev
-vault server -dev &
-
-sudo vault operator init
-
-vault write secret/hello fooo=worl ter=pio "tert tas"=lkjlk  a=b c=d
-vault read -format=json secret/hello
-
+mv -f vault consul /usr/local/bin/ 
 
 cat > /etc/systemd/system/consul.service <<CONSULSYSTEMD
 [Unit]
@@ -45,7 +28,7 @@ Description=Consul
 Documentation=https://www.consul.io/
 
 [Service]
-ExecStart=/usr/bin/consul agent -server -ui -data-dir=/tmp/consul -bootstrap-expect=1 -node=vault -bind=10.0.0.17 -config-dir=/etc/consul.d/
+ExecStart=/usr/local/bin/consul agent -server -ui -data-dir=/tmp/consul -bootstrap-expect=1 -node=vault -bind=10.0.0.17 -config-dir=/etc/consul.d/
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
 
@@ -91,3 +74,24 @@ LimitNOFILE=65536
 WantedBy=multi-user.target
 VAULTSYSTEMD
 
+
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable consul
+sudo systemctl enable vault
+sudo systemctl start consul
+sudo systemctl start vault
+
+sudo systemctl status consul
+sudo systemctl status vault
+
+
+export VAULT_ADDR=http://127.0.0.1:8200
+
+vault --version
+sudo consul agent -dev &
+vault server -dev &
+sudo vault operator init
+vault write secret/hello fooo=worl ter=pio "tert tas"=lkjlk  a=b c=d
+vault read -format=json secret/hello
